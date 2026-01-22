450c19ef85397bf7fe19183eb3e168ed13aeea74
RJ-08 <ranjitsamalarj@gmail.com>
2026-01-22 18:50:44 +0530

    Update App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./components/Login";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <div>
+      <Login />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
